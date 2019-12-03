bfd03c4655319a14a6ee7d94c5a2e7503194f303
Demarius Edwards <demarius.edwards@gmail.com>
2019-12-03 20:18:08 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -108,7 +108,8 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     programmer_hash[:alan_kay][:known_for]
+     alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for][1] = alans_new_info
      
 end
 
